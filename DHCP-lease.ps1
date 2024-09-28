e984fa9b6bab66fbd1d24c948e4e0a6c16d71577
unknown <torops4750@gmail.com>
2024-09-28 18:20:07 +0200

    Legger til dhcp-lease.ps1

---
+++ DHCP-lease.ps1
@@ -0,0 +1,32 @@
+﻿
+#increase buffer limit to unlimited
+$FormatEnumerationLimit=-1
+$file = "C:\scripts\output\DHCP Scopes.csv"
+
+#Poll AD to get list of DHCP Servers
+$Servers = Get-DhcpServerInDC
+
+#create empty array
+$report = @()
+
+#Loop
+foreach($Server in $Servers){
+
+#Poll DHCP servers to get scopes
+$scope = Get-DhcpServerv4Scope -ComputerName $Server.IPAddress
+
+
+$lease = Get-DhcpServerv4Lease -ScopeId $scope.scopeId
+#Select what objects are added to the array
+$report += [pscustomobject]@{
+
+
+    'Scope ID' = $scope.ScopeId
+    'IPAddress' = $lease.ipaddress
+    'HostName' = $Server.DnsName
+    'ClientID' = $lease.clientid
+    'State' = $scope.state 
+}
+
+}
+$report | Export-Csv $file
